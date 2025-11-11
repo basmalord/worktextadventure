86ee70f2927b3e03147d12996e71ef9678701070
basmalord <theobasmalord@live.co.uk>
2025-11-11 13:13:18 +0000

    initial
    This is the initial commit.

--- Passage/Scripts/passage.gd
+++ Passage/Scripts/passage.gd
@@ -14,12 +14,12 @@ var data: Dictionary
 func _ready():
 	load_passage()
 
-func load_passage():
-	if passage_json_file_location == null:
+func load_passage(): #This loads the json file associated with the passage we want to access.
+	if passage_json_file_location == null: #This checks if there is data harboured in the file location variable.
 		print("ERROR: NO JSON FILE LOCATION SET")
 		return
-	var passage_json_file = FileAccess.open(passage_json_file_location, FileAccess.READ)
-	if passage_json_file:
+	var passage_json_file = FileAccess.open(passage_json_file_location, FileAccess.READ) #This tells godot to access the file for reading purposes.
+	if passage_json_file: #Below is the process for turning the string data of the file into json data, allowing us to take the information in it and apply it to the game scene, thus making a passage.
 		var text = passage_json_file.get_as_text()
 		var new_json = JSON.new()
 		var json_data = new_json.parse(text)
@@ -33,12 +33,12 @@ func load_passage():
 		print(input_strings)
 
 
-func _on_input_box_text_submitted(player_input_text: String) -> void:
-	if player_input_text == "":
+func _on_input_box_text_submitted(player_input_text: String) -> void: #This fires when the input box is selected by the player and they hit 'ENTER'.
+	if player_input_text == "": #This checks whether the text inputted is empty
 		return
-	if valid_inputs == {}:
+	if valid_inputs == {}: #This checks whether the passage the player is on has valid inputs.
 		print("ERROR: NO VALID INPUTS DETECTED IN PASSAGE")
 		return
-	if input_strings.find(player_input_text.to_lower()) != -1:
+	if input_strings.find(player_input_text.to_lower()) != -1: #This checks whether the text inputted by the player matches any of the inputs associated with the passage.
 		passage_json_file_location = passage_json_files_folder + valid_inputs[player_input_text]["passage_name"] + ".txt"
 		load_passage()
---
+++ README.md
@@ -0,0 +1,35 @@
+# **WorkTextAdventure**
+
+
+
+**Data Structure:**
+
+Uses JSON to store data pertaining to passages. JSON is a dictionary-based coding language that allows for the storing of values under key names. The key names and values for our project, along with a brief description of each, are as follows:
+
+
+
+1. passage\_name : a string of text that describes the passage (naming convention tbd)
+2. visual\_name : a string of text that describes the visual element of the passage (naming convention tbd)
+3. passage\_text : a string of text that will be presented to the player whenever the passage is loaded
+4. inputs : the valid inputs the player can input in order to load a new passage, each input is a dictionary which contains a key, "passage\_name" that provides a string of text that describes the passage that will be loaded upon the inputting of the corresponding input.
+
+
+
+Note: passage\_name and visual\_name must correspond to the name under which corresponding files are saved, this allows the string data within the JSON file to be used to load the desired passages and visuals.
+
+
+
+**Resources:**
+
+
+
+JSON beginner's guide (syntax and formatting super simple!): https://stackoverflow.blog/2022/06/02/a-beginners-guide-to-json-the-data-format-for-the-internet/
+
+
+
+JSON validator (provides testing for your JSON files to see if they can be read as such, will also highlight where and how syntax errors occur):
+
+https://stackoverflow.blog/2022/06/02/a-beginners-guide-to-json-the-data-format-for-the-internet/
+
+
+
